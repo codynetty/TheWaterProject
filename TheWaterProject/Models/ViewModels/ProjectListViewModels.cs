0f7f8fb2601dd2e65c1e6c7fe9a3c282a4c09a9c
Cody Nettesheim <codynetty@gmail.com>
2024-03-19 16:49:27 -0600

    Add project files.

---
+++ TheWaterProject/Models/ViewModels/ProjectListViewModels.cs
@@ -0,0 +1,10 @@
+﻿namespace TheWaterProject.Models.ViewModels
+{
+    public class ProjectListViewModels
+    {
+        public IQueryable<Project> Projects { get; set; }
+
+        public PaginationInfo PaginationInfo { get; set; } = new PaginationInfo();
+
+    }
+}
